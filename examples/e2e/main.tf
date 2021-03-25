76089cc504284e8ff81d300a81e9da15e37f6e30
Andreas Thomas <dev@chronark.com>
2021-03-25 11:04:52 +0100

    feat(alias): expose existing aliases for each project
    Use `vercel_project.XYZ.alias` to access them.

--- examples/e2e/main.tf
+++ examples/e2e/main.tf
@@ -11,28 +11,10 @@ provider "vercel" {
   token = "wsByP9ptGqn7snGvvY00aDzn"
 }
 
-data "vercel_user" "me" {}
-
 resource "vercel_project" "my_project" {
   name = "mercury-via-terraform"
   git_repository {
     type = "github"
     repo = "chronark/mercury"
   }
-
-
-
-}
-
-resource "vercel_env" "env" {
-  project_id = vercel_project.my_project.id
-  type       = "secret"
-  key        = "key"
-  value      = vercel_secret.my_secret.id
-  target     = ["production", "preview", "development"]
-}
-
-resource "vercel_secret" "my_secret" {
-  name  = "my_secret"
-  value = "super secret"
 }
