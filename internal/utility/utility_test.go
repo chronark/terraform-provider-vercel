81132f7f813687ca45323b7b644f25ae4c726f21
Dave Perrett <dave@recurser.com>
2021-08-15 09:33:03 +0900

    Add and remove domains when a project is updated.

---
+++ internal/utility/utility_test.go
@@ -0,0 +1,27 @@
+package utility
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestDifference(t *testing.T) {
+	tests := []struct {
+		input1    []string
+		input2    []string
+		expected  []string
+	}{
+		{[]string{"a", "b", "c"}, []string{"a", "b", "d"}, []string{"c"}},
+		{[]string{"a", "b", "d"}, []string{"a", "b", "c"}, []string{"d"}},
+		{[]string{"a", "b", "c"}, []string{"d", "e", "f"}, []string{"a", "b", "c"}},
+		{[]string{"a", "b", "c"}, []string{}, []string{"a", "b", "c"}},
+	}
+
+	for _, test := range tests {
+		actual := Difference(test.input1, test.input2)
+
+		if !reflect.DeepEqual(test.expected, actual) {
+			t.Errorf("expected: %v, received: %v", test.expected, actual)
+		}
+	}
+}
